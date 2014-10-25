6c57910164350888f3e4635015bd19cc93cb1ae3
Dhaval Kolapkar <dhaval.kolapkar@outlook.com>
2014-10-25 14:05:28 -0700

    Updated dropbox API UI

---
+++ application/lib/dropbox-node.js
@@ -0,0 +1,349 @@
+var pathLib = require('path')
+  , querystring = require('querystring')
+  , fs = require('fs')
+  , escapePath = require('./util/escape-path')
+  , stringifyParams = require('./util/stringify-params')
+  , errors = require('./errors')
+  , EventEmitter = require('events').EventEmitter
+  , request = require('request')
+  , API_URI = 'https://api.dropbox.com/1'
+  , CONTENT_API_URI = 'https://api-content.dropbox.com/1';
+
+
+// Returns a Dropbox Client, through which API calls can be made.
+var DropboxClient = exports.DropboxClient =
+  function(consumer_key, consumer_secret, access_token, access_token_secret, options) {
+    options = options || {};
+    this.consumer_key = consumer_key;
+    this.consumer_secret = consumer_secret;
+    this.access_token = access_token || undefined;
+    this.access_token_secret = access_token_secret || undefined;
+    this.root = options.sandbox ? 'sandbox' : 'dropbox';
+  }
+
+
+// Creates a request to the API with OAuth credentials
+DropboxClient.prototype.request =
+  function(method, uri, optargs, body, callback) {
+    if (typeof body === 'function') callback = body, body = undefined;
+    optargs = optargs || {};
+    var oauth = {
+      consumer_key: this.consumer_key
+    , consumer_secret: this.consumer_secret
+    , token: optargs.token || this.access_token
+    , token_secret: optargs.secret || this.access_token_secret
+    };
+
+    var requestOptions = { uri: uri, oauth: oauth };
+    if (body) {
+      if (method === 'get') {
+        requestOptions.headers = { Range: body };
+      } else {
+        requestOptions.body = body;
+      }
+    }
+
+    return request[method](requestOptions, callback ?
+      function(err, res, body) {
+        if (err) return callback(err);
+        var contentType = res.headers['content-type'];
+
+        // check if the response body is in JSON format
+        if (contentType === 'application/json' ||
+            contentType === 'text/javascript') {
+          body = JSON.parse(body);
+          if (body.error) {
+            var err = new Error(body.error);
+            err.statusCode = res.statusCode;
+            return callback(err);
+          }
+
+        } else if (errors[res.statusCode]) {
+          var err = new Error(errors[res.statusCode]);
+          err.statusCode = res.statusCode;
+          return callback(err);
+        }
+
+        // check for metadata in headers
+        if (res.headers['x-dropbox-metadata']) {
+          var metadata = JSON.parse(res.headers['x-dropbox-metadata']);
+        }
+
+        callback(null, body, metadata);
+      } : undefined);
+};
+
+
+// Convenience methods
+['get', 'post', 'put'].forEach(function(method) {
+  DropboxClient.prototype['_' + method] = function(uri, optargs, body, callback) {
+    return this.request(method, uri, optargs, body, callback);
+  };
+});
+
+
+// Fetches an access token and access token secret pair based on the user's
+// email and password. As well as being stored internally, the key pair is
+// returned to the callback in case the application developer requires it.
+DropboxClient.prototype.getAccessToken = function(email, pwd, cb) {
+  // Validate email and pwd.
+  if (!email || !pwd) return cb(Error('Invalid arguments. Please provide ' +
+                                      'a valid email and password.'));
+
+  var uri = API_URI + '/token?' +
+    querystring.stringify({email: email, password: pwd});
+  var self = this;
+  this._get(uri, {}, function(err, data) {
+    if (err) return cb(err);
+    // Store the key pair and fire callback.
+    self.access_token = data.token;
+    self.access_token_secret = data.secret;
+    cb(null, data.token, data.secret);
+  });
+}
+
+
+// Retrieves information about the user's account as a JSON response.
+DropboxClient.prototype.getAccountInfo = function(optargs, cb) {
+  if (typeof optargs == 'function') cb = optargs, optargs = {};
+  var uri = API_URI + '/account/info/' +
+    (optargs.status_in_response ? '?status_in_response=' +
+    optargs.status_in_response : '');
+  this._get(uri, optargs, cb);
+}
+
+
+// Create a new Dropbox account.
+DropboxClient.prototype.createAccount = function(email, first_name, last_name
+                                                 , password, optargs, cb) {
+  if (typeof optargs == 'function') cb = optargs, optargs = {};
+  var params = {
+    email: email
+    , first_name: first_name
+    , last_name: last_name
+    , password: password
+    , status_in_response: optargs.status_in_response
+  }
+  var uri = API_URI + '/account?' + querystring.stringify(params);
+
+  this._get(uri, {}, cb);
+}
+
+
+// Retrieves contents of a file specified by path argument, relative to
+// user's Dropbox root.
+DropboxClient.prototype.getFile = function(path, optargs, cb) {
+  if (typeof optargs == 'function') cb = optargs, optargs = {};
+  else if (!optargs) optargs = {};
+  var uri = CONTENT_API_URI + '/files/' + this.root + '/' + escapePath(path) +
+    (optargs.rev ? '?rev=' + optargs.rev : '');
+
+  return this._get(uri, optargs, optargs.range, cb);
+}
+
+
+// Uploads contents of a file specified by file to the remote path
+DropboxClient.prototype.putFile = function(file, path, optargs, cb) {
+  if (typeof optargs == 'function') cb = optargs, optargs = {};
+  var uri = CONTENT_API_URI + '/files_put/' + this.root + '/' + escapePath(path) +
+    '?' + stringifyParams(optargs);
+  var self = this;
+
+  fs.readFile(file, function(err, data) {
+    if (err) return cb(err);
+    self.request('put', uri, optargs, data, cb);
+  });
+}
+
+
+// Uploads contents to the specified path
+DropboxClient.prototype.put = function(content, path, optargs, cb) {
+  if (typeof optargs == 'function') cb = optargs, optargs = {};
+  var uri = CONTENT_API_URI + '/files_put/' + this.root + '/' + escapePath(path) +
+    '?' + stringifyParams(optargs);
+
+  this._put(uri, optargs, content, cb);
+}
+
+
+// Gets metadata of file/folder specified by path relative to user's
+// Dropbox root.
+DropboxClient.prototype.getMetadata = function(path, optargs, cb) {
+  if (typeof optargs == 'function') cb = optargs, optargs = {};
+  var uri = API_URI + '/metadata/' + this.root + '/' + escapePath(path) + '?' +
+    stringifyParams(optargs);
+
+  this._get(uri, optargs, cb);
+}
+
+
+// Downloads a minimized jpeg thumbnail for a photo. See
+// https://www.dropbox.com/developers/docs#thumbnails for a list of
+// valid size specifiers.
+DropboxClient.prototype.getThumbnail = function(path, optargs, cb) {
+  if (typeof optargs == 'function') cb = optargs, optargs = {};
+  optargs = optargs || {};
+  var uri = CONTENT_API_URI + '/thumbnails/' + this.root + '/' + escapePath(path) +
+    '?' + stringifyParams(optargs);
+
+  return this._get(uri, optargs, cb);
+}
+
+
+// Creates and gets a link to the specified file or folder.
+// See https://www.dropbox.com/developers/docs/api#shares for explanation
+// of arguments.
+DropboxClient.prototype.share = function(path, optargs, cb) {
+  if (typeof optargs == 'function') cb = optargs, optargs = {};
+  optargs = optargs || {};
+  var uri = API_URI + '/shares/' + this.root + '/' + escapePath(path) +
+    '?' + stringifyParams(optargs);
+
+  return this._post(uri, optargs, cb);
+}
+
+
+// Creates and gets a direct link to the specified file.
+// See https://www.dropbox.com/developers/docs/api#media for explanation
+// of arguments.
+DropboxClient.prototype.media = function(path, optargs, cb) {
+  if (typeof optargs == 'function') cb = optargs, optargs = {};
+  optargs = optargs || {};
+  var uri = API_URI + '/media/' + this.root + '/' + escapePath(path) +
+    '?' + stringifyParams(optargs);
+
+  return this._post(uri, optargs, cb);
+}
+
+
+// Copies a file or folder to a new location.
+// See https://www.dropbox.com/developers/docs#fileops-copy for explanation
+// of arguments.
+DropboxClient.prototype.copy = function(from_path, to_path, optargs, cb) {
+  if (typeof optargs == 'function') cb = optargs, optargs = {};
+  optargs.root = this.root;
+  if (!optargs.from_copy_ref) optargs.from_path = from_path;
+  optargs.to_path = to_path;
+  var uri = API_URI + '/fileops/copy?' + querystring.stringify(
+    {root: this.root, from_path: from_path, to_path: to_path});
+
+  this._get(uri, optargs, cb);
+}
+
+
+// Creates a folder relative to the user's Dropbox root.
+// See https://www.dropbox.com/developers/docs#fileops-create-folder
+// for explanation of arguments.
+DropboxClient.prototype.createFolder = function(path, optargs, cb) {
+  if (typeof optargs == 'function') cb = optargs, optargs = {};
+  var uri = API_URI + '/fileops/create_folder?' +
+    querystring.stringify({root: this.root, path: path});
+
+  this._get(uri, optargs, cb);
+}
+
+
+// Deletes a file or folder.
+// See https://www.dropbox.com/developers/docs#fileops-delete for
+// explanation of arguments.
+DropboxClient.prototype.deleteItem = function(path, optargs, cb) {
+  if (typeof optargs == 'function') cb = optargs, optargs = {};
+  var uri = API_URI + '/fileops/delete?' +
+    querystring.stringify({root: this.root, path: path});
+
+  this._get(uri, optargs, cb);
+}
+
+
+// Moves a file or folder.
+// See https://www.dropbox.com/developers/docs#fileops-move for
+// explanation of arguments.
+DropboxClient.prototype.move = function(from_path, to_path, optargs, cb) {
+  if (typeof optargs == 'function') cb = optargs, optargs = {};
+  var uri = API_URI + '/fileops/move?' +
+    querystring.stringify({root: this.root, from_path: from_path, to_path: to_path});
+
+  this._get(uri, optargs, cb);
+}
+
+
+// Searches a folder
+// See https://www.dropbox.com/developers/reference/api#search
+DropboxClient.prototype.search = function(path, query, optargs, cb) {
+  if (typeof optargs === 'function') cb = optargs, optargs = {};
+  optargs.query = query;
+  var uri = API_URI + '/search/' + this.root + '/' + escapePath(path) + '?' +
+    stringifyParams(optargs);
+
+  this._get(uri, optargs, cb);
+}
+
+
+// Keep up with changes.
+// See https://www.dropbox.com/developers/reference/api#delta
+DropboxClient.prototype.delta = function(cursor, optargs, cb) {
+  var cursorType = typeof cursor;
+  if (cursorType === 'function') cb = cursor, optargs = {};
+  else if (typeof optargs === 'function') {
+    cb = optargs;
+    if (cursorType === 'object') {
+      optargs = cursor;
+      cursor = null;
+    } else {
+      optargs = {}
+    }
+  }
+
+  var uri = API_URI + '/delta' +
+    (cursor ? '?' + querystring.stringify({cursor: cursor}) : '')
+
+  this._post(uri, optargs, cb);
+}
+
+
+// Continously stream changes through DropboxClient#delta
+// Returns an event emitter that has `pause()` and `resume()` methods.
+// It emits `reset` events on detta resets, and `data` events with
+// parameters `path` and `metadata` on new delta changes.
+DropboxClient.prototype.changesStream = function(cursor, optargs) {
+  optargs = optargs || {};
+  var ee = new EventEmitter();
+  var iid;
+  var self = this;
+
+  function getDelta() {
+    self.delta(cursor, optargs, function(err, data) {
+      if (err) { return ee.emit('error', err); }
+
+      // only emit changes if cursor was given
+      if (cursor) {
+        if (data.reset) {
+          ee.emit('reset');
+        }
+
+        for (var i = 0, len = data.entries.length; i < len; i++) {
+          var e = data.entries[i];
+          ee.emit('data', e[0], e[1]);
+        }
+      }
+
+      cursor = data.cursor;
+      if (data.has_more) {
+        ee.resume();
+      }
+    });
+  }
+
+  ee.resume = function() {
+    getDelta();
+    clearInterval(iid);
+    iid = setInterval(getDelta, 300000); // 5 minutes
+  };
+  ee.resume();
+
+  ee.pause = function() {
+    clearInterval(iid);
+  };
+
+  return ee;
+}
---
+++ application/readme.md
@@ -0,0 +1,196 @@
+# dropbox-node
+
+An OAuth-enabled Node.js client for working with the Dropbox API.
+
+## DISCLAIMER
+
+This project has been deprecated in favor of [Dropbox's official JavaScript library](https://github.com/dropbox/dropbox-js).
+
+## Installation
+
+dropbox-node depends on [node-oauth](http://github.com/ciaranj/node-oauth).
+
+To install via npm
+
+    npm install dropbox-node
+
+To install by hand, download the module and create a symlink in `~/.node_libraries`
+
+    $ ln -s /path/to/dropbox-node/ ~/.node_libraries/dropbox-node
+
+## Usage
+
+To start, grab a consumer key and secret from [dropbox.com/developers](https://dropbox.com/developers).
+
+### Object construction and access key pair retrieval
+First construct a DropboxClient object, passing in the consumer key and secret.
+
+    var dropbox = new DropboxClient(consumer_key, consumer_secret)
+
+Before calling any Dropbox API methods, an access token pair must be obtained. This can be done one of two ways:
+
+  1. If the access token and secret are known a priori, they can be passed directly into the DropboxClient constructor.
+
+        var dropbox = new DropboxClient(consumer_key, consumer_secret,
+                                        access_token, access_token_secret)
+
+  2. Otherwise, `getAccessToken` must be called in order to initialize the OAuth credentials.
+
+        dropbox.getAccessToken(dropbox_email, dropbox_password, callback)
+
+The callback given to `getAccessToken` takes an error object, an access token, and an access token secret (see example below). **Please note that users' passwords should never be stored.** It is best to acquire a token once and then use it for all subsequent requests.
+
+### Calling API methods
+
+dropbox-node provides methods covering [each of the Dropbox API methods](https://www.dropbox.com/developers/docs). For example, to fetch and print the display name and email address associated with your account:
+
+    dropbox.getAccountInfo(function (err, data) {
+      if (err) console.log('Error: ' + err)
+      else console.log(data.display_name + ', ' + data.email)
+    })
+
+Note that (at least at the start of a user's session) a valid access token pair must be obtained prior to interacting with the rest of the Dropbox API. This means that the API methods must be invoked within the callback passed to `getAccessToken` unless it is guaranteed that `getAccessToken` was called previously. As an example of this latter case, if building a web app, one could call API methods directly in a route that can only be accessed after going through a sign-in phase in which a call to `getAccessToken` is made.
+
+Here we upload a file and remotely move it around before deleting it.
+
+    dropbox.getAccessToken(email, pwd, function (err, token, secret) {
+      // Upload foo.txt to the Dropbox root directory.
+      dropbox.putFile('foo.txt', 'foo.txt', function (err, data) {
+        if (err) return console.error(err)
+
+        // Move it into the Public directory.
+        dropbox.move('foo.txt', 'Public/foo.txt', function (err, data) {
+          if (err) return console.error(err)
+
+          // Delete the file.
+          dropbox.deleteItem('Public/foo.txt', function (err, data) {
+            if (err) console.error(err.stack)
+          })
+        })
+      })
+    })
+
+For a more practical example, check out this [walkthrough of building a simple Dropbox file browser](http://evanmeagher.net/2010/10/dropbox-file-browser).
+
+### Optional arguments
+
+Optional arguments (as specified in the [Dropbox API documentation](https://www.dropbox.com/developers/docs)) can be given to API methods via an argument object.
+
+For example, here we call `getAccountInfo` and direct the API to include the HTTP status code in the response.
+
+    dropbox.getAccountInfo({ status_in_response: true }, callback)
+
+Each method (except `getAccessToken`) can optionally take an access token and an access token secret as strings. This is the one way to get around having to call `getAccessToken` in cases where a valid key pair is known.
+
+For example, here we fetch the metadata about the Dropbox root directory, passing in an explicit key pair stored in variables.
+
+    dropbox.getMetadata('', { token: token, secret: secret }, callback)
+
+## API
+
+### new DropboxClient()
+
+### DropboxClient#getAccessToken(email, password, callback(err, access_token, access_token_secret))
+
+Fetches an access token and secret based on the email and password given. Stores the token and secret in the DropboxClient instance and calls the callback them.
+
+### DropboxClient#getAccountInfo([optargs], callback(err, accountInfo))
+https://www.dropbox.com/developers/reference/api#account-info
+
+Gets account information from the client.
+
+### DropboxClient#createAccount(email, first_name, last_name, password, [optargs], callback(err, accountInfo))
+
+Creates a new Dropbox account.
+
+### DropboxClient#getFile(path, [optargs], [callback(err, body)])
+https://www.dropbox.com/developers/reference/api#files-GET
+
+Retrieves a file specified by the path. `callback` will be called with a possible error and the buffer of the contents of the file. This method also returns a readable stream that can be used to pipe the contents.
+
+```js
+dropboxClient('mybigfile.mpeg').pipe(fs.createWriteStream('localbigfile.mpeg');
+```
+
+`optargs` can also have a `rev` field to specify the revision of the file to download, and `range` for [HTTP Range Retrieval Requests](http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.35.2).
+
+```js
+// download the first 1024 byte
+dropboxClient('file.zip', { range: 'bytes=0-1024'}, function(err, data) {
+  console.log(data.length); // 1024. that is if the file is at least 1024 bytes
+});
+```
+
+### DropboxClient#putFile(filepath, remotepath, [optargs], callback(err, metadata))
+https://www.dropbox.com/developers/reference/api#files_put
+
+Uploads a file specified by `filepath` to `remotepath` on Dropbox. Dropbox currently does not support streaming uploads, and the max upload is 150 MB. `optargs` can also take additional fields `overwrite` and `parent_rev`.
+
+### DropboxClient#put(contents, remotepath, [optargs], callback(err, metadata))
+o
+Similar to `putFile()` but places `contents` into a created file at `remotepath`. `contents` can be a buffer or string.
+
+### DropboxClient#getMetadata(path, [optargs], callback(err, metadata))
+https://www.dropbox.com/developers/reference/api#metadata
+
+Gets metadata of file/folder specified by `path`. `optargs` can have fields `hash`, `list`, `include_deleted` and `rev`.
+
+### DropboxClient#delta([cursor], [optargs], callback(err, changes))
+https://www.dropbox.com/developers/reference/api#delta
+
+Keeps up with changes from a client's Dropbox. `changes` is an array of arrays with first element as the path and second as metadata.
+
+### DropboxClient#changesStream([startingCursor], [optargs])
+Convenient method that provides a more friendly API to `delta()`. Returns an event emitter that emits `data` events with `path` and `metadata` parameters on changes to the client's Dropbox. Also can emit `reset` and `error` events. The returned event emitter also has a `pause()` and `resume()` methods.
+
+### DropboxClient#search(folderpath, query, [optargs], callback(err, results))
+https://www.dropbox.com/developers/reference/api#search
+
+Searches `folderpath` for files matching `query`. `results` is an array of metadata. `optargs` can take `file_limit` and `include_deleted`.
+
+### DropboxClient#getThumbnail(filepath, [optargs], [callback(err, body, metadata)])
+https://www.dropbox.com/developers/reference/api#thumbnails
+
+Downloads a thumbnail image located at `filepath`. Like `getFile()`, the `callback` can get buffered data or the returned readable stream can be piped. `optargs` can take `format` and `size` fields.
+
+### DropboxClient#shares(path, [optargs], [callback(err, link)])
+https://www.dropbox.com/developers/reference/api#shares
+
+Creates and gets a link to file/folder specified by `path`.
+
+### DropboxClient#media(filepath, [optargs], [callback(err, link)])
+https://www.dropbox.com/developers/reference/api#media
+
+Creates and gets a direct link to file specified by `filepath`.
+
+### DropboxClient#copy(from_path, to_path, [optargs], callback)
+https://www.dropbox.com/developers/reference/api#fileops-copy
+
+Copies a file. `from_copy_ref` field can be given in `optargs` to use it instead of `from_path`.
+
+### DropboxClient#createFolder(path, [optargs], callback(err, metadata))
+https://www.dropbox.com/developers/reference/api#fileops-create-folder
+
+Creates a folder at the given path.
+
+### DropboxClient#deleteItem(path, [optargs], callback(err, metadata))
+https://www.dropbox.com/developers/reference/api#fileops-delete
+
+Deletes file or folder from path.
+
+### DropboxClient#move(from_path, to_path, [optargs], callback(err, metadata))
+https://www.dropbox.com/developers/reference/api#fileops-move
+
+Moves a file to another path.
+
+
+## Testing
+
+dropbox-node depends on [jasmine-node](http://github.com/mhevery/jasmine-node) for testing. Note that the currently-implemented tests are trivial, due to a lack of a way to effectively mock the Dropbox API.
+
+Run specs with `node specs.js` from the root `dropbox-node` directory.
+
+## TODO
+* Improve test coverage.
+* Improve documentation.
+* Add ability to interact with application sandboxes.
