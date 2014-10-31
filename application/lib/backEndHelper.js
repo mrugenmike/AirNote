228e211cc8f57a2a11555d64c8e7f5db9df175b0
vaibhavb <vaibhavb@surveymonkey.com>
2014-10-30 18:56:25 -0700

    Addes get user info flow

---
+++ application/lib/backEndHelper.js
@@ -0,0 +1,17 @@
+var querystring = require('querystring');
+var http = require('http');
+var request = require('request');
+var host = 'localhost';
+var port = 8080;
+var endpoint = 'api/users';
+
+var backEndClient = {
+    performGetUserInfoRequest: function(endpoint, code, callback) {
+        request.get(endpoint, {
+            headers: { Authorization: code },
+            json: true
+        }, callback);
+}
+}
+
+module.exports = backEndClient
