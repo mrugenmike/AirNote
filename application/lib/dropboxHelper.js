6729b24460743670f3f656931170d0df18156ce5
Dhaval Kolapkar <dhaval.kolapkar@outlook.com>
2014-11-03 00:05:59 -0800

    isAuthenticated updated to true

--- application/lib/dropboxHelper.js
+++ application/lib/dropboxHelper.js
@@ -22,7 +22,7 @@ var dropBoxClient = {
         });
     },
     isAuthenticated: function(req,res){
-  return false;
+  return true;
     },
     authenticateUser: function(req,res){
         var csrfToken = this.generateCSRFToken();
