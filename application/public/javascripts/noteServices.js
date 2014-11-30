0980d7fadf05e10993da11ac432b78bd85c35238
sowmya89 <sowmya.prakash1@gmail.com>
2014-11-30 15:35:18 -0800

    accessing note title changes

--- application/public/javascripts/noteServices.js
+++ application/public/javascripts/noteServices.js
@@ -17,12 +17,14 @@ angular.module('AirNoteApp.noteServices', []).
             });
         }
 
-        notesAPI.createNote = function(){
+        notesAPI.createNote = function(title, contents){
+            console.log(title);
+            console.log(contents);
             return $http({
                 headers: {'Authorization': 'Bearer L5WpAIC9mn0AAAAAAAAAkXl4dATdr26BLATTHl-HSg9Ezn1EXD5K6-_STgilxSv3'},
                 url: 'http://localhost:8080/api/notes',
-                data: {"title": "Vab",
-                    "content": "This is test content!",
+                data: {"title": title,
+                    "content": contents,
                     "userId": "351441185"
                 },
                 method: 'POST'
@@ -36,15 +38,24 @@ angular.module('AirNoteApp.noteServices', []).
                 });
         }
 
-        notesAPI.updateNote = function(){
+        notesAPI.updateNote = function(title, contents, noteId){
             return $http({
-
-                })
+                headers: {'Authorization': 'Bearer L5WpAIC9mn0AAAAAAAAAkXl4dATdr26BLATTHl-HSg9Ezn1EXD5K6-_STgilxSv3'},
+                url: 'http://localhost:8080/api/notes',
+                data: {"title": title,
+                    "content": contents,
+                    "userId": "351441185",
+                    "noteId": "547a7c0c0364648b83bc8170"
+                },
+                method: 'PUT'
+            })
         }
 
         notesAPI.deleteNote = function(){
             return $http({
-
+                headers: {'Authorization': 'Bearer L5WpAIC9mn0AAAAAAAAAkXl4dATdr26BLATTHl-HSg9Ezn1EXD5K6-_STgilxSv3'},
+                url: 'http://localhost:8080/api/notes/351441185/54562835a8262eb8960d7f04',
+                method: 'DELETE'
             })
         }
 
--- application/public/javascripts/notesController.js
+++ application/public/javascripts/notesController.js
@@ -1,6 +1,6 @@
 var controller = angular.module('AirNoteApp.controllers', ['ngCookies']);
 
-controller.config(function($httpProvider) {
+controller.config(function ($httpProvider) {
     //Enable cross domain calls
     $httpProvider.defaults.useXDomain = true;
 
@@ -8,39 +8,66 @@ controller.config(function($httpProvider) {
     delete $httpProvider.defaults.headers.common['X-Requested-With'];
 });
 
-controller.controller('notesController', function($scope, notesAPIservice,$cookieStore) {
-        $scope.nameFilter = null;
-        $scope.notesList = [];
-        $scope.username=null;
-        $scope.noteId=null;
-        $scope.note=null;
-        notesAPIservice.getUserInfo().success(function (response) {
-            $scope.username =response.display_name
+controller.controller('notesController', function ($scope, notesAPIservice, $cookieStore) {
+    $scope.nameFilter = null;
+    $scope.notesList = [];
+    $scope.username = null;
+    $scope.noteId = null;
+    $scope.note = null;
 
-        }).error(function(data, status, headers, config){
-            $scope.username="oops we had an error!"
-        });
+    $scope.create = function() {
+        //$scope.msg = 'clicked';
+        console.log('Clicked!!');
+        var title = document.getElementById("createTitle").value;
+        var contents = document.getElementById("createContent").value;
 
-        notesAPIservice.listNotes().success(function(response){
-            $scope.notesList = response;
-        }).error(function(data){
-            $scope.notesList=[]
-        });
-
-        notesAPIservice.createNote().success(function(response){
+        notesAPIservice.createNote(title, contents).success(function (response) {
             $scope.noteId = response.noteId;
+            console.log('note id is' + $scope.noteId);
         });
+    }
 
-        notesAPIservice.fetchNote().success(function(response){
-            $scope.note = response;
-        });
+        notesAPIservice.getUserInfo().success(function (response) {
+        $scope.username = response.display_name
 
-        notesAPIservice.updateNote().success(function(response){
-            $scope.note = response;
-        });
-
-        notesAPIservice.deleteNote().success(function(response){
-
-        });
+    }).error(function (data, status, headers, config) {
+        $scope.username = "oops we had an error!"
     });
+
+    notesAPIservice.listNotes().success(function (response) {
+        $scope.notesList = response;
+        var notesDecorators = [];
+        var myNote = null;
+        $scope.notesList.forEach(function (note) {
+            console.log(note.title);
+            myNote = new DecoratedNote(note);
+            //myNote.setCurrentNote();
+            notesDecorators.push(myNote);
+        });
+        console.log(notesDecorators.length);
+        $scope.noteDecoratorList = notesDecorators;
+    }).error(function (data) {
+        $scope.notesList = []
+    });
+
+    function DecoratedNote(note) {
+        this.note = note;
+        this.setCurrentNote = function () {
+            console.log("Click done at" + note.title);
+            $scope.clickedNoteTitle = note.title;
+        };
+    }
+
+    notesAPIservice.fetchNote().success(function (response) {
+        $scope.note = response;
+    });
+
+    notesAPIservice.updateNote().success(function (response) {
+        $scope.note = response;
+    });
+
+    notesAPIservice.deleteNote().success(function (response) {
+
+    });
+});
 
