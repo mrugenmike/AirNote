5291b642188e08297b6895af43d2e35006ffbd34
Mrugen Deshmukh <mrugenmike@gmail.com>
2014-12-10 14:39:30 -0800

    added template placeholder

--- services/src/main/java/com/airnote/services/reminder/ReminderEmailImpl.java
+++ services/src/main/java/com/airnote/services/reminder/ReminderEmailImpl.java
@@ -3,15 +3,18 @@ package com.airnote.services.reminder;
 import com.sendgrid.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ReminderEmailImpl implements ReminderEmailService {
     private final SendGrid sendGrid;
+    private final String template;
 
     @Autowired
-    public ReminderEmailImpl(@Qualifier("sendGrid") SendGrid sendGrid){
+    public ReminderEmailImpl(@Qualifier("sendGrid") SendGrid sendGrid,@Value("email.template")String template){
         this.sendGrid = sendGrid;
+        this.template = template;
     }
 
     public void send(String fromEmail, String toEmail, String subject, String body) {
@@ -20,7 +23,7 @@ public class ReminderEmailImpl implements ReminderEmailService {
         email.setFrom(fromEmail);
         email.setSubject(subject);
         email.setText(body);
-        email.setHtml("<b>"+body+"</b>");
+        email.setHtml(template);
         try {
             SendGrid.Response response = sendGrid.send(email);
             System.out.println(response.getMessage());
