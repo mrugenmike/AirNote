5fe7c844dbc8f22633471887f20381e13cff4648
Mrugen Deshmukh <mrugenmike@gmail.com>
2014-12-10 15:58:16 -0800

    fixing CORS issue

--- application/app.js
+++ application/app.js
@@ -11,6 +11,9 @@ var users = require('./routes/users');
 
 var app = express();
 
+var cors = require("cors")
+app.use(cors())
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
